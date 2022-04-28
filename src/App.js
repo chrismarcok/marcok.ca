a12e2970283de89c79c7ad2a4d09d98e5a7b8d6d
Chris Marcok <chris.marcok@gmail.com>
2022-04-28 16:06:00 -0400

    React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="jumbotron">
+      <div className="middle-container">
+        <div className="main-box">
+          <div className="box-header">
+            <h1 className="header-name">Chris Marcok</h1>
+            <h3 className="header-title">Software Developer</h3>
+            <h3 className="header-location">Toronto, Ontario, Canada</h3>
+            <a href="https://www.github.com/chrismarcok" target="_blank" rel="noreferrer" className="item-text">GitHub</a>{" | "}
+            <a href="https://www.linkedin.com/in/chrismarcok" target="_blank" rel="noreferrer" className="item-text">LinkedIn</a>{" | "}
+            <a href="/documents/Christopher_Marcok_Resume.pdf" target="_blank" rel="noreferrer" className="item-text">Resume</a>{" | "}
+            <a href="/documents/Academic_History.pdf" target="_blank" rel="noreferrer" className="item-text">Transcript</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
